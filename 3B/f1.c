d26eed05d82d39147e36b99bbe9d710dba363dfb
Vardhan_Kirti <114055813+VardhanKirti@users.noreply.github.com>
2025-05-06 21:45:49 +0530

    Add files via upload

---
+++ 3B/f1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+int age, contact, roll;
+printf("Enter your age: ");
+scanf("%d",&age);
+printf("enter your contact info");
+scanf("%d", &contact);
+printf("enter your roll");
+scanf("%d", &roll);
+return 0;
+}
---
+++ 3B/f2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+int age, contact, roll;
+writef("Enter your age: ");
+readf("%d",&age);
+writef("enter your contact info");
+readf("%d", &contact);
+writef("enter your roll");
+readf("%d", &roll);
+return 0;
+}
